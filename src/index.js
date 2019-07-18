49915d49723f6ab11ddb8282e97f35502899f25d
Leon Li <la2li@uwaterloo.ca>
2019-07-18 01:33:22 -0400

    connect to server

--- src/index.js
+++ src/index.js
@@ -10,3 +10,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+fetch('http://localhost:8000/current-time')
+  .then(response => response.json())
+  .then(console.log)
+  .catch(console.error)
